2f17c0666930f218932baff036596890104761c2
Tyler Bateman <43663440+tyler-bateman@users.noreply.github.com>
2020-06-05 20:59:45 -0700

    Once again changed my mind on how the data should be parsed

--- parsing.py
+++ parsing.py
@@ -8,10 +8,10 @@
 #        - Thus, all values in the list will be a number from 1-11
 #
 #
-# Chords will be sampled from the data at 4 every 250 ms, which is 16th notes
+# Chords will be sampled from the data at 4 every 250 ms, which is sixtennth notes
 # at the interonset interval that Westergaard calls 'too fast to be useful'. I
-# figure that it is unlikely that any chordal information would be skipped at
-# that rate.
+# figure that it is unlikely that any significant chordal information would be
+# skipped at that rate.
 # Of course, most chords will last longer than 250 ms. Because this task relates
 # to the transitions between chords, any time a chord is identical to the
 # preceding chord (according the the rules above), it will be ignored.
@@ -44,7 +44,8 @@ ids = list(train_data.keys())
 # deltas: A set containing integers representing the distance in semitones from
 #         the bass note to each other note in the chord, normalized to one octave
 def normalizeChord(interval):
-    notes = [label[2][1] for label in sorted(interval)]
+    notes = [label[2][1] for label in interval]
+    notes = sorted(notes)
     bass = notes.pop(0) % 12
     deltas = set([(note - bass) % 12 for note in notes])
     deltas.discard(0)
@@ -59,60 +60,38 @@ def normalizeChord(interval):
 def getChords(id):
     labels = train_data[id][1]
 
+    phrases = []
+
     qualities = []
     transitions = []
     durations = []
 
 
     moment = labels.begin()
-    prevChord = None
-
+    prevBass = None
     #Parse individual chords
     while moment < labels.end():
-        c = None
+
         if len(labels[moment]) > 0:
             c = normalizeChord(labels[moment])
-            if c != prevChord:
-                q = c[1]
-                t = ((c[0] - (transitions[-1] if len(transitions) > 0 else 0)) + 12) % 12
-                if not prevChord is None and t == 0 and q.issubset(prevChord[1]):
-                    durations[-1][1] = moment
-                elif not prevChord is None and t == 0 and q.issuperset(prevChord[1]):
-                    qualities[-1] = q
-                    durations[-1][1] = moment
-                else:
-                    qualities.append(q)
-                    transitions.append(t)
-                    durations.append([moment, moment])
-            else:
-                durations[-1][1] = moment
+
+            if len(qualities) > 0:
+                transitions.append((c[0] - prevBass) % 12)
+            qualities.append(c[1])
+            prevBass = c[0]
+        else:
+            if len(transitions) > 0:
+                phrases.append((transitions, tuple(qualities)))
+            qualities = []
+            transitions = []
+            durations= []
 
         moment += spc
-        prevChord = c
-
-    #remove non-chords and convert qualities to tuples
-    idx = 0
-    while idx < len(qualities):
-        qualities[idx] = tuple(sorted(qualities[idx]))
-        if len(qualities[idx]) == 0:
-            qualities.pop(idx)
-            durations.pop(idx)
-            transitions.pop(idx)
-            idx -= 1
-
-        idx += 1
-
-    #Split into phrases
-    phrases = []
-    startIndex = 0
-    for i in range(len(qualities) - 1):
-        if durations[i + 1][0] - durations[i][1] > fs * 2:
-            phrases.append((transitions[startIndex + 1 : i + 1], qualities[startIndex : i + 1]))
-            startIndex = i + 1
 
     return phrases
 
 # Returns a list of all musical phrases in MusicNet
+
 def getAllPhrases():
     phrases = []
     for id in train_data:
