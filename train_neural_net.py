af23306f2179617df7bb1484ac68c4f0f1b5051e
Tyler Bateman <43663440+tyler-bateman@users.noreply.github.com>
2020-06-07 22:19:55 -0700

    Create train_neural_net.py

---
+++ train_neural_net.py
@@ -0,0 +1,11 @@
+from textgenrnn import textgenrnn
+
+
+def main():
+    textgen = textgenrnn()
+    textgen.reset()
+    textgen.train_from_file('data/train_data.txt', num_epochs = 1)
+    textgen.save('models/nn_weights.hdf5')
+
+if __name__ = '__main__':
+    main()
