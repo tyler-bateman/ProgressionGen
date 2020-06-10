b71732f27cbfbff5938167c4ab13163628e78647
Tyler Bateman <43663440+tyler-bateman@users.noreply.github.com>
2020-06-09 17:09:46 -0700

    Write generated chords to file

--- neural_net_gen.py
+++ neural_net_gen.py
@@ -1,8 +1,13 @@
 from textgenrnn import textgenrnn
 
+TEMPS = [0.3, 0.5, 1.0]
 
 def main():
     textgen = textgenrnn('models/nn_weights.hdf5')
-    textgen.generate_samples()
+    for tmp in TEMPS:
+        output = open('output/nn_chords_{}'.format(tmp), 'w')
+        chords = textgen.generate(10, temperature = tmp)
+        output.write(chords)
+        
 
 main()
