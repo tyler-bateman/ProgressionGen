0e95bf1e36c72ca626c9af04364effdd1ab704e3
Tyler Bateman <batemat2@wwu.edu>
2020-06-09 17:35:36 -0700

    progression generation with neural nets now creates midi files

--- neural_net_gen.py
+++ neural_net_gen.py
@@ -1,13 +1,17 @@
 from textgenrnn import textgenrnn
+from chordstomidi import write_file
 
 TEMPS = [0.3, 0.5, 1.0]
 
 def main():
     textgen = textgenrnn('models/nn_weights.hdf5')
     for tmp in TEMPS:
-        output = open('output/nn_chords_{}'.format(tmp), 'w')
-        chords = textgen.generate(10, temperature = tmp)
-        output.write(chords)
+        filename = 'output/nn_chords_{}'.format(tmp)
+        textgen.generate_to_file(filename, n = 10, temperature = tmp)
+        progs = open(filename, 'r').readlines()
+        for i in range(len(progs)):
+            p = progs[i].strip().split(' ')
+            write_file('midi_files/nn_{}_{}.mid'.format(tmp, i), p)
         
 
 main()
