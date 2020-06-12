9af1fac0389ba26618b051ecf769382766ceac73
Tyler Bateman <43663440+tyler-bateman@users.noreply.github.com>
2020-06-11 22:47:34 -0700

    Implemented human evaluation calculations

---
+++ human_eval_results.py
@@ -0,0 +1,31 @@
+import os
+
+model_types = ['1-gram', '2-gram', '3-gram', '4-gram', '1-order_markov', '2-order_markov', '3-order_markov', 'nn_0.5', 'nn_0.75', 'nn_1.0']
+
+evals = os.listdir('evals')
+with open('human_eval_files.txt', 'r') as f:
+    filenames = [name.strip() for name in f.readlines()]
+
+def calculateScores():
+    counts = dict.fromkeys(set(model_types), 0)
+    for eval in evals:
+        with open('evals/{}'.format(eval), 'r') as f:
+            scores = [s.strip() for s in f.readlines()]
+        for i in range(len(filenames)):
+            for type in model_types:
+                if type in filenames[i]:
+                    counts[type] += int(scores[i])
+    scores = {}
+    for item in counts.items():
+        scores[item[0]] = item[1] / (len(evals) * 4)
+    return scores
+
+
+
+def main():
+    scores = calculateScores()
+    for item in scores.items():
+        print(item)
+
+if __name__ == '__main__':
+    main()
